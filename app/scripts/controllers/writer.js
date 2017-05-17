591b2a454389f9a00bfe8899f16dea22e4a1ba85
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-05-17 13:03:42 +0800

    WIP: HTML cleanup of profile.html

--- app/scripts/controllers/writer.js
+++ app/scripts/controllers/writer.js
@@ -8,9 +8,33 @@
  * Controller of the ngmaterialApp
  */
 angular.module('ngmaterialApp')
-.controller('WriterCtrl', function($scope) {
-  $scope.editIntro = function () {
-    $scope.templatePath = 'edit-intro.html';
-    $scope.intro = true;
+.controller('WriterCtrl', function($scope, $mdDialog) {
+  $scope.profile = {
+    firstname: 'Jigno Alfred',
+    lastname: 'Venezuela',
+    headline: 'JavaScript Cultist | Web Developer',
+  }
+
+  $scope.editIntro = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'views/writer/edit-intro.html',
+      parent: angular.element(document.body),
+      scope: $scope,
+      preserveScope: true,
+      targetEvent: ev,
+      clickOutsideToClose: true,
+      fullscreen: $scope.customFullscreen
+    })
+  };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
   }
 });
