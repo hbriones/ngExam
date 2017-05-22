b46fb48744bf0e42a44644b76c16ebb9eae0dba5
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-05-22 15:19:01 +0800

    Testing of QuillJS

--- app/scripts/controllers/writer.js
+++ app/scripts/controllers/writer.js
@@ -9,6 +9,38 @@
  */
 angular.module('ngmaterialApp')
 .controller('WriterCtrl', function($scope, $mdDialog) {
+
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike', 'link'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['video', 'formula', 'image'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']                                         // remove formatting button
+  ];
+
+  var options = {
+    theme: 'snow',
+    modules: {
+      toolbar: toolbarOptions,
+      formula: true          // Include formula module (needs extra css + Katex Script)
+    }
+  }
+
+  var container = document.getElementById('customQuill');
+  var editor = new Quill(container, options);
+
   $scope.profile = {
     firstname: '',
     lastname: '',
